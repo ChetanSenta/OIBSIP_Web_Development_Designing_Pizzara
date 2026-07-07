3215570f4c69a833a741fe2a12e0ae3a6ea5b606
ChetanSenta <chetansenta11@gamil.com>
2026-07-07 18:41:24 +0530

    notification fixed

---
+++ frontend/src/utils/toastOptions.js
@@ -0,0 +1,11 @@
+export const toastOptions = {
+  position: "top-right",
+  autoClose: 3000,
+  hideProgressBar: true,
+  closeButton: false,
+  pauseOnHover: false,
+  pauseOnFocusLoss: false,
+  pauseOnVisibilityChange: false,
+  closeOnClick: false,
+  draggable: false,
+};
